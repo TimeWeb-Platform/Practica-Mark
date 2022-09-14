62642bb26b01dd14af1efa7c4efcefeed67329ba
MarkAnderson101001 <marco.alonso@timeweb.com>
2022-09-13 23:06:26 -0500

    Agregar archivos de proyecto.

---
+++ Eventos/Domain/Evento.cs
@@ -0,0 +1,12 @@
+﻿namespace Eventos.Domain
+{
+    public class Evento
+    {
+        public int EventoID       { get; set; }
+        public int UsuarioID      { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public string? Latitud    { get; set; }
+        public string? Longitud   { get; set; }
+        public string Origen      { get; set; }
+    }
+}
---
+++ Usuarios/Domain/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+
+namespace Usuarios.Domain
+{
+    public class User
+    {
+        public int UsuarioID            { get; set; }
+        public string Nombre            { get; set; }
+        public string ApP               { get; set; }
+        public string ApM               { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int RazonSocialID        { get; set; }
+    }
+}
